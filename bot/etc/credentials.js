fa2c6e821b818e9dc993cdc3d2f85084221a64c4
Michael Belyakov <mv-belyakov@yandex.ru>
2018-08-03 09:43:17 +0300

    ws2

--- bot/etc/credentials.js
+++ bot/etc/credentials.js
@@ -1,4 +1,5 @@
 module.exports = {
     token: '411911421:AAEsXjw4qwKpwBmdDhnH7NzOYxZKUhmwqFY',
-    admins: [43962820]
+    admins: [43962820],
+    ws_port: 3501
 }
--- bot/telegraf.js
+++ bot/telegraf.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-const {token} = require('./etc/credentials.js')
+const {token, ws_port} = require('./etc/credentials.js')
 const Telegraf = require('telegraf')
 const Telegram = require('telegraf/telegram')
 const bot = new Telegraf(token)
 const tg = new Telegram(token)
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({port: 3501})
+const wss = new WebSocket.Server({port: ws_port})
 const redis = require('redis')
 const bluebird = require('bluebird')
 bluebird.promisifyAll(redis.RedisClient.prototype)
 const redisc = redis.createClient()
 
 require('./modules/admin.js')(bot, redisc, tg)
-require('../ws/websocket.js')(redisc, tg)
+require('../ws/websocket.js')(redisc, wss)
 //require('./modules/menu.js')(bot, redisc)
 //require('./modules/payments.js')(redisc, tg)
 //
